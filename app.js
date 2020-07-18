831ce0d1791203ef621f84618a75e6ab012891ce
france17 <francegdev@gmail.com>
2020-07-18 19:42:40 +0800

    lowered the sizes

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post("/submit", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("Server Started");
 });
